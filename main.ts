a53d1ab0db56dae2e4efc1296db0362a866f8046
aalonso0132 <aalonsopersonal@gmail.com>
2022-06-14 16:00:12 +0200

    First commit. Package setted, main.ts created and ESLint configured with AirBnB

---
+++ main.ts
@@ -0,0 +1,16 @@
+/* eslint-disable no-console */
+// Require the necessary discord.js classes
+import { Client, Intents } from 'discord.js';
+
+require('dotenv').config();
+
+// Create a new client instance
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+
+// When the client is ready, run this code (only once)
+client.once('ready', () => {
+  console.log('Ready!');
+});
+
+// Login to Discord with your client's token
+client.login(process.env.token);
