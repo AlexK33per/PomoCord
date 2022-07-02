9c63b10c1fda5143b94b7e898712e543141577cd
aalonso0132 <aalonsopersonal@gmail.com>
2022-07-02 23:14:18 +0200

    Fixed some eslint errors

--- main.ts
+++ main.ts
@@ -1,79 +1,81 @@
 /* eslint-disable no-console */
 // Require the necessary discord.js classes
 import DiscordJS, { Client, Intents } from 'discord.js';
-
 import dotenv from 'dotenv';
+
 dotenv.config();
 
 // Create a new client instance
-const client = new Client({ 
+const client = new Client({
   intents: [
     Intents.FLAGS.GUILDS,
-    Intents.FLAGS.GUILD_MESSAGES
-  ]
+    Intents.FLAGS.GUILD_MESSAGES,
+  ],
 });
 
 // When the client is ready, run this code (only once)
 client.once('ready', () => {
   console.log('Ready!');
-  let commands = client.application?.commands;
+  const commands = client.application?.commands;
 
   commands?.create({
     name: 'test',
-    description: 'test'
-  })
+    description: 'test',
+  });
 
   commands?.create({
     name: 'start',
     description: 'Starts the Pomodoro Technique with the given parameters',
     options: [{
-        name: 'worktime',
-        description: 'The amount for each Pomodoro',
-        type: DiscordJS.Constants.ApplicationCommandOptionTypes.NUMBER,
-        required: true
-      },
-      {
-        name: 'stoptime',
-        description: 'Amount of time you want to rest between Pomodoros',
-        type: DiscordJS.Constants.ApplicationCommandOptionTypes.NUMBER,
-        required: true
-      },
-      {
-        name: 'repetitions',
-        description: 'Number of repetitions you want to do of one Pomodoro and and Stop',
-        type: DiscordJS.Constants.ApplicationCommandOptionTypes.NUMBER,
-        required: true
-      }
-    ]
-  })
+      name: 'worktime',
+      description: 'The amount for each Pomodoro',
+      type: DiscordJS.Constants.ApplicationCommandOptionTypes.NUMBER,
+      required: true,
+    },
+    {
+      name: 'stoptime',
+      description: 'Amount of time you want to rest between Pomodoros',
+      type: DiscordJS.Constants.ApplicationCommandOptionTypes.NUMBER,
+      required: true,
+    },
+    {
+      name: 'repetitions',
+      description: 'Number of repetitions you want to do of one Pomodoro and and Stop',
+      type: DiscordJS.Constants.ApplicationCommandOptionTypes.NUMBER,
+      required: true,
+    },
+    ],
+  });
 });
 
 client.on('messageCreate', (message) => {
-  if(!message.author.bot) {
+  if (!message.author.bot) {
     console.log(`${message} received`);
     message.reply({
-      content: 'Your message was received'
+      content: 'Your message was received',
     });
   }
-})
+});
 
 client.on('interactionCreate', async (interaction) => {
-  if(!interaction.isCommand()) {
-    return
-  } else {
-    const { commandName, options } = interaction;
-    switch(commandName) {
-      case 'start':
-        interaction.reply({
-          content: 'You have started the program successfully'
-        });
-        break;
-      case 'test':
-        interaction.reply({
-          content: 'test'
-        });
-        break;
-    }
+  if (!interaction.isCommand()) {
+    // eslint-disable-next-line no-useless-return
+    return;
+  }
+  const { commandName } = interaction;
+  switch (commandName) {
+    case 'start':
+      interaction.reply({
+        content: 'You have started the program successfully',
+      });
+      break;
+    case 'test':
+      interaction.reply({
+        content: 'test',
+      });
+      break;
+    default:
+      break;
   }
 });
 
